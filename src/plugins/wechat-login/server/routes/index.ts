329c3eb27a23f549e38a04d82f925f5f2927c6a0
huangpeilin <huangpeilin@qunjielong.com>
2023-04-22 21:21:48 +0800

    feat: 优化请求路径

--- src/plugins/wechat-login/server/routes/index.ts
+++ src/plugins/wechat-login/server/routes/index.ts
@@ -1,7 +1,7 @@
 export default [
   {
     method: 'GET',
-    path: '/',
+    path: '/wechat',
     handler: 'wechatController.index',
     config: {
       auth: false,
@@ -10,7 +10,7 @@ export default [
   },
   {
     method: 'POST',
-    path: '/',
+    path: '/wechat',
     handler: 'wechatController.handleWechatMessage',
     config: {
       auth: false,
